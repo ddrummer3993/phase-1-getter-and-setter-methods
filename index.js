df0ed607a269bfe26e981502f6b8d9e9af64c7c7
David Drummer <david.b.drummer@gmail.com>
2022-03-12 16:41:29 -0500

    commit tree 7edde7a321e87e459df0f65e95041ebe18c6d4bb

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your Circle class here
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    get diameter() {
+        return this.radius * 2;
+    };
+    get circumference() {
+        return 2 * Math.PI * this.radius;
+    };
+    get area() {
+        return Math.PI * (Math.pow(this.radius, 2));
+    };
+
+    set diameter(newDiameter) {
+        return this.radius = newDiameter / 2;
+    };
+    set circumference(newCircumference) {
+        return this.radius = newCircumference / (2 * Math.PI);
+    };
+    set area(newArea) {
+        return this.radius = Math.sqrt(newArea / Math.PI);
+    };
+};
+
+const circle = new Circle(6);
+console.log(circle.radius);
+console.log(circle.diameter);
+console.log(circle.circumference);
+console.log(circle.area);
+
+
+
+
